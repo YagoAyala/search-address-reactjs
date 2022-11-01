52695e93b1699b26a31a507554c722b242908331
YagoAyala <yago.ayala@outlook.com>
2022-11-01 16:37:03 -0300

    feat: created all css

--- search-adress/src/App.js
+++ search-adress/src/App.js
@@ -1,7 +1,32 @@
+import { FiSearch } from 'react-icons/fi';
+import './style.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>TESTE PROJETO</h1>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input 
+        type="text"
+        placeholder="Digite o seu CEP..."
+        />
+
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 89251-360</h2>
+
+        <span>Rua max nicolau</span>
+        <span>Complemento: alguma coisa</span>
+        <span>Vila lalau</span>
+        <span>Campo grande - ms</span>
+      </main>
+
     </div>
   );
 }
